456b8b39982eee6cc733e2f5c91ae6e18fa222ff
rodolfonepomuceno <rodolfonepomuceno24@gmail.com>
2014-09-20 09:45:54 -0400

    Fragmento Credenciales
    Popup de credenciales

--- MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto1.java
+++ MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto1.java
@@ -1,9 +1,13 @@
 package com.example.rodolfo.universidad;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
 
 /**
  * Created by Rodolfo on 28/8/14.
@@ -13,6 +17,41 @@ public class Recinto1 extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recinto1);
+
+        //Control de los Toast
+        final Context context = getApplicationContext();
+        final int duration = Toast.LENGTH_SHORT;
+
+     /*   //Objetos asociados a los botones
+        final ImageButton button_estudiantes = (ImageButton) findViewById(R.id.imageButton_estudiante);
+
+        button_estudiantes.setOnClickListener(new ImageButton.OnClickListener() {
+
+            CharSequence text = "Estudiantes";
+
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });*/
+
+
+
+    }
+
+    public void credenciales (View view)
+    {
+        CharSequence text = "Recinto Santo Domingo";
+
+        final Context context = getApplicationContext();
+        final int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent i = new Intent(this, Credenciales.class );
+        startActivity(i);
     }
 
 }
+
