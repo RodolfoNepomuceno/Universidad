da5ad3e7a7214b4bbeb95e461fe43b1b3eadd8c1
RodolfoVargas <rodolfo_nepomuceno@hotmail.com>
2014-09-24 11:07:07 -0400

    Resolver Inconveniente de Dialog
    Al extender de la clase ActionBarActivity en vez de Activity, es
    posible encontrar el metodo getSupportFragmentManager()

--- MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto.java
+++ MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -20,6 +21,9 @@ public class Recinto extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recinto);
+
+        DialogFragment dialog = new Credenciales();
+        dialog.show(getSupportFragmentManager(), "dialog");
 
     //Establece el Activity solo en vertical
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
--- MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto1.java
+++ MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recinto1.java
@@ -1,18 +1,25 @@
 package com.example.rodolfo.universidad;
 
-import android.app.Activity;
+//import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
 
 
 /**
  * Created by Rodolfo on 28/8/14.
+ *
  */
-public class Recinto1 extends Activity
+
+
+public class Recinto1 extends ActionBarActivity //Activity
 {
         private static final String TAG = "dialog";
+
+            //Crea una instancia de la clase Credenciales
+            public DialogFragment dialog = new Credenciales();
 
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,7 @@ public class Recinto1 extends Activity
         //Control de los Toast
         final Context context = getApplicationContext();
         final int duration = Toast.LENGTH_SHORT;
+
 
      /* //Objetos asociados a los botones
         final ImageButton button_estudiantes = (ImageButton) findViewById(R.id.imageButton_estudiante);
@@ -51,6 +59,8 @@ public class Recinto1 extends Activity
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+
+        dialog.show(getSupportFragmentManager(), "dialog");
 
         //Intent i = new Intent(Recinto1.this,Credenciales.class);
         //startActivity(i);
