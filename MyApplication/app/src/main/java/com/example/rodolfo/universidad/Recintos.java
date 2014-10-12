789639d2aa2b72273897967081350405b7e7da33
RodolfoVargas <rodolfo_nepomuceno@hotmail.com>
2014-10-12 15:25:18 -0400

    Limpieza del codigo
    Limpieza de codigos comentados

--- MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recintos.java
+++ MyApplication/app/src/main/java/com/example/rodolfo/universidad/Recintos.java
@@ -3,7 +3,6 @@ package com.example.rodolfo.universidad;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -18,40 +17,6 @@ public class Recintos extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recinto);
-
-        //Establece el Activity solo en vertical
-       // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        //Control de los Toast
-
-
-   //Objetos asociados a los botones
-   /* final ImageButton button_recinto1 = (ImageButton) findViewById(R.id.imageButton_r1);
-    final ImageButton button_recinto2 = (ImageButton) findViewById(R.id.imageButton_r2);*/
-
-
-   /* button_recinto1.setOnClickListener(new ImageButton.OnClickListener() {
-
-        @Override
-        public void onClick(View view) {
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-        }
-    }
-    );
-
-    button_recinto2.setOnClickListener(new ImageButton.OnClickListener() {
-
-        CharSequence text = "Recintos Santo Domingo";
-
-        @Override
-        public void onClick(View view) {
-            setContentView(R.layout.recinto2);
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-    });*/
 }
     //Funcion del Metodo OnClick llamado en el layout
     public void boton_recinto1 (View view)
@@ -81,28 +46,6 @@ public class Recintos extends ActionBarActivity {
         Intent i = new Intent(this, Recinto2.class );
         startActivity(i);
     }
-
-
-   /* @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        // TODO Auto-generated method stub
-
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP:
-                Toast.makeText(this, "Boton de Volumen Up presionado",
-                        Toast.LENGTH_SHORT).show();
-                return true;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                Toast.makeText(this, "Boton de Volumen Down presionado",
-                        Toast.LENGTH_SHORT).show();
-                return true;
-            case KeyEvent.KEYCODE_BACK:
-                Toast.makeText(this, "Boton Atras",
-                        Toast.LENGTH_SHORT).show();
-                return true;
-        }
-        return super.onKeyUp(keyCode, event);
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
